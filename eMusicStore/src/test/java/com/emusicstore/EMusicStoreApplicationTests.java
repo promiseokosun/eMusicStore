620ae77a81a20b3da86e02b40a64c35477c205da
promise <promiseokosun@gmail.com>
2020-06-06 12:43:50 +0100

    Initial Commit

---
+++ eMusicStore/src/test/java/com/emusicstore/EMusicStoreApplicationTests.java
@@ -0,0 +1,13 @@
+package com.emusicstore;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EMusicStoreApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
